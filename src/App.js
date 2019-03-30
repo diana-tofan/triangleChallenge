672fe8cb29fd4619d45688388f651c39f0fcaeb1
Diana Tofan <diana_tofan@ymail.com>
2019-03-30 15:58:45 +0100

    Initialize app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>We are amaaaaazing</div>
+    );
+  }
+}
+
+export default App;
