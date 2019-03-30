4157ad2b7ecf542fba887f55f2e7b3d6cad9fd72
Diana Tofan <diana_tofan@ymail.com>
2019-03-30 16:34:53 +0100

    Updates

---
+++ precache-manifest.f066bacb0e68bcb0f59594e0b4a36f64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00d40644ad0a9c5a964d",
+    "url": "/Triangle-challenge/static/css/main.df52f678.chunk.css"
+  },
+  {
+    "revision": "00d40644ad0a9c5a964d",
+    "url": "/Triangle-challenge/static/js/main.be6a6bbe.chunk.js"
+  },
+  {
+    "revision": "2a36218eff8902842620",
+    "url": "/Triangle-challenge/static/js/runtime~main.11df2014.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Triangle-challenge/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "de4747cc1f8cef0d3c6dba019836c247",
+    "url": "/Triangle-challenge/index.html"
+  }
+];
